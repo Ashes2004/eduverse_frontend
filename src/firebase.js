e74be14bcaa4a347cb5f77321b2148db0706b58d
Ashes Das <dasashes713@gmail.com>
2024-12-18 09:30:55 +0530

    small change

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,16 @@
-// firebase-config.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAgvhe8SFjfvLGakEN6ZFRD8qKrGvh7A9A",
 
-    authDomain: "myclass-6cf84.firebaseapp.com",
-  
-    projectId: "myclass-6cf84",
-  
-    storageBucket: "myclass-6cf84.appspot.com",
-  
-    messagingSenderId: "520136229549",
-  
-    appId: "1:520136229549:web:73a8c2529e61a8aa817c24",
-  
-    measurementId: "G-6LXQP37V9P"
-  
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
